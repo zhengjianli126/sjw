e98493f0740690a6a5f5ccb6915c25c77c435dbc
zhengjianli <zhengjianli126@qq.com>
2018-03-19 09:07:40 +0800

    错误代码监控

--- src/main.js
+++ src/main.js
@@ -8,6 +8,34 @@ import '@/locale';
 import 'iview/dist/styles/iview.css';
 import VueI18n from 'vue-i18n';
 import util from '@/libs/util';
+import fundebug from 'fundebug-javascript';
+fundebug.apikey = "05ab3a370500b3fd7096247fe49761ad95d9b6b3bcac4cd9ecf8dff3fca0ae54";
+
+// 代码错误监控
+function formatComponentName(vm)
+{
+  if (vm.$root === vm) return 'root';
+
+  var name = vm._isVue ? (vm.$options && vm.$options.name) || (vm.$options && vm.$options._componentTag) : vm.name;
+  return (name ? 'component <' + name + '>' : 'anonymous component') + (vm._isVue && vm.$options && vm.$options.__file ? ' at ' + (vm.$options && vm.$options.__file) : '');
+
+}
+
+Vue.config.errorHandler = function(err, vm, info)
+{
+  var componentName = formatComponentName(vm);
+  var propsData = vm.$options && vm.$options.propsData;
+
+  fundebug.notifyError(err,
+  {
+      metaData:
+      {
+          componentName: componentName,
+          propsData: propsData,
+          info: info
+      }
+   });
+};
 Vue.prototype.util = util;
 Vue.use(VueI18n);
 Vue.use(iView);
